f0a622fb035df43ae160e13be8b00634dba08558
meganhug11 <mhughes1@chicagobooth.edu>
2015-05-06 19:04:32 -0500

    Form
    Completed New Form

--- app/controllers/zodiacs_controller.rb
+++ app/controllers/zodiacs_controller.rb
@@ -1,3 +1,13 @@
 class ZodiacsController < ApplicationController
 
+  def new_form
+  end
+
+  def create_row
+    @sign = params[:symbol]
+    @animal = params[:animal]
+    @horoscope = params[:horoscope]
+  end
+
+
 end
